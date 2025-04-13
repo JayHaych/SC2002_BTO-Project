c0a71f5d25af22ce8c6504be3860a92a91c3c983
damien1432 <damientan30@gmail.com>
2025-04-13 13:02:03 +0800

    edited applicant controller

--- src/Entity/CurrentUser.java
+++ src/Entity/CurrentUser.java
@@ -1,14 +1,50 @@
-package Entity;
+/*this entity class is to let the controllers know who is currently using the application
+ * it will be used for visibility checks (if this applicant can view the project or not)
+ * as such, it will not include setter methods
+ */
 
-public class CurrentUser {
-    String name;
-    String NRIC;
-    int age;
-    String martialStatus;
-    String password;
+ package Entity;
 
-    public CurrentUser(){
-        
-    }
-}
+ public class CurrentUser {
+     private String name;
+     private String NRIC;
+     private int age;
+     private String maritalStatus; // corrected spelling from "martialStatus"
+     private String password;
+ 
+     // Default constructor
+     public CurrentUser() {
+     }
+ 
+     // Parameterized constructor
+     public CurrentUser(String name, String NRIC, int age, String maritalStatus, String password) {
+         this.name = name;
+         this.NRIC = NRIC;
+         this.age = age;
+         this.maritalStatus = maritalStatus;
+         this.password = password;
+     }
+ 
+     // Getters
+     public String getName() {
+         return name;
+     }
+ 
+     public String getNRIC() {
+         return NRIC;
+     }
+ 
+     public int getAge() {
+         return age;
+     }
+ 
+     public String getMaritalStatus() {
+         return maritalStatus;
+     }
+ 
+     public String getPassword() {
+         return password;
+     }
+ 
+ }
 
