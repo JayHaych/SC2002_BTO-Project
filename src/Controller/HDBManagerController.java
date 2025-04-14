2142798b30c573d05ffd854e68b86eb105858bd7
meganchuaa <megansarahchua@gmail.com>
2025-04-14 21:40:10 +0800

    added manager ui

---
+++ src/Controller/HDBManagerController.java
@@ -0,0 +1,95 @@
+package Controller;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import Entity.BTOProject;
+
+public class HDBManagerController {
+    private boolean applicationApproved;
+    private boolean registrationApproved;
+    private boolean withdrawalApproved;
+    
+    public HDBManagerController(boolean applicationApproved, boolean registrationApproved, boolean withdrawalApproved){
+        this.applicationApproved = applicationApproved;
+        this.registrationApproved = registrationApproved;
+        this.withdrawalApproved = withdrawalApproved;
+    }
+
+    public void createProject(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Project Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Neighbourhood: ");
+        String neighbourhood = sc.nextLine();
+
+        System.out.print("Number of 2-room flats: ");
+        int twoRoom = sc.nextInt();
+
+        System.out.print("Number of 3-room flats: ");
+        int threeRoom = sc.nextInt();
+
+        sc.nextLine();
+        System.out.print("Opening Date: ");
+        String openingDate = sc.nextLine();
+
+        System.out.print("Closing Date: ");
+        String closingDate = sc.nextLine();
+
+        System.out.print("Manager Name: ");
+        String manager = sc.nextLine();
+
+        System.out.print("Number of officer slots: ");
+        int officerSlots = sc.nextInt();
+
+        BTOProject project = new BTOProject(name, neighbourhood, twoRoom, threeRoom, openingDate, closingDate, manager, officerSlots, true);
+
+        System.out.println(" Project created successfully!");
+        
+    }
+
+    public void editProject(){
+        viewAllProjects();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter project name: ");
+        String projectName = sc.nextLine();
+        sc.nextLine();
+
+
+    }
+
+    public void deleteProject(){
+
+    }
+
+    public void setVisibility(){
+
+    }
+
+    public void viewAllProjects(){
+
+    }
+
+    public void viewMyProjects(){
+
+    }
+
+    public boolean approveRegistration(){
+        return registrationApproved;
+    }
+
+    public boolean approveApplication(){
+        return applicationApproved;
+    }
+
+    public boolean approveWithdrawal(){
+        return withdrawalApproved;
+    }
+
+    public String generateReport(){
+
+    }
+
+
+
+}
--- src/UI/HDB_Manager_UI.java
+++ src/UI/HDB_Manager_UI.java
@@ -1,10 +1,87 @@
 package UI;
 
-public class HDB_Manager_UI 
-{
-    public static void display()
-    {
-        System.out.println("___________________________________________________________________________________________");
-        System.out.println("Welcome to the HDB Manager UI!");
+import java.util.Scanner;
+
+import Controller.HDBManagerController;
+
+public class HDB_Manager_UI {
+    
+    public static void display(){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        HDBManagerController controller = new HDBManagerController(true, true, true);
+
+        while (true) {
+            System.out.println("___________________________________________________________________________________________");
+            System.out.println("Welcome to the HDB Manager UI!");
+            System.out.println("1. Create new BTO project");
+            System.out.println("2. Edit existing BTO project");
+            System.out.println("3. Delete BTO project");
+            System.out.println("4. Set project visibility");
+            System.out.println("5. View all projects");
+            System.out.println("6. View my projects");
+            System.out.println("7. Generate report");
+            System.out.println("8. Approve registration");
+            System.out.println("9. Approve application");
+            System.out.println("10. Approve withdrawal");
+            System.out.println("11. Logout and quit");
+            System.out.print("Select option: ");
+
+            choice = sc.nextInt();
+            String report;
+
+            switch (choice) {
+                case 1:
+                    controller.createProject();
+                    break;
+                case 2:
+                    controller.editProject();
+                    break;
+                case 3:
+                    controller.deleteProject();
+                    break;
+                case 4:
+                    controller.setVisibility();
+                    break;
+                case 5:
+                    controller.viewAllProjects();
+                    break;
+                case 6:
+                    controller.viewMyProjects();
+                    break;
+                case 7:
+                    report = controller.generateReport();
+                    System.out.println(report);
+                    break;
+                case 8:
+                    if (controller.approveRegistration()){
+                        System.out.println("Registration approved!");
+                    } else{
+                        System.out.println("Registration rejected");
+                    }
+                    break;
+                case 9:
+                    if (controller.approveApplication()){
+                        System.out.println("Application approved!");
+                    } else {
+                        System.out.println("Application rejected");
+                    }
+                    break;
+                case 10:
+                    if (controller.approveWithdrawal()){
+                        System.out.println("Withdrawal approved!");
+                    } else{
+                        System.out.println("Withdrawal rejected");
+                    }
+                    break;
+                case 11:
+                    System.out.println("Logging out...");
+                    System.out.println("Thank you!");
+                    return;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        }
     }
 }
